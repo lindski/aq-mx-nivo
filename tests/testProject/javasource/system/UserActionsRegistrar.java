3b2b5c7a044dc5b7b0c3cf7a9a2ca45f4badd756
Iain Lindsay <iain.lindsay@auraq.com>
2023-02-20 10:48:13 +0000

    add sample for choropleth and geomap

--- tests/testProject/javasource/system/UserActionsRegistrar.java
+++ tests/testProject/javasource/system/UserActionsRegistrar.java
@@ -7,6 +7,8 @@ public class UserActionsRegistrar
   public void registerActions(IActionRegistrator registrator)
   {
     registrator.bundleComponentLoaded();
+    registrator.registerUserAction(nivotestdata.actions.GetFileContentsFromResources.class);
+    registrator.registerUserAction(nivotestdata.actions.GetPath.class);
     registrator.registerUserAction(system.actions.VerifyPassword.class);
   }
 }
